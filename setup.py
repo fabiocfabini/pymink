469c4b7a58c89de1a7c1af5e7d0db163ca4ca022
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-09-17 21:40:27 +0100

    update remote repo

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+from importlib.metadata import entry_points
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, find_packages
 from version import __version__, __rootdir__
 
 
@@ -13,14 +14,15 @@ setup(
     description='A Python library to perform N Gram based text analysis.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    py_modules=list(map(lambda x: str(x).split('/')[-1].split('.')[0], Path(__rootdir__).rglob('*.py'))),
-    package_dir={'': __rootdir__},
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Information Analysis',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
@@ -32,6 +34,12 @@ setup(
     extras_require={
         'dev': [
             'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            f'{__rootdir__}.gn = {__rootdir__}.cli:guess_number',
+            f'{__rootdir__}.sn = {__rootdir__}.cli:square_number',
         ],
     },
 )
